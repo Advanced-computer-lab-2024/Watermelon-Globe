7ec690c1414028b1b982878fca5adf4eeac7850b
Abuel3la <167132038+Abuel3la@users.noreply.github.com>
2024-10-05 14:43:03 +0300

    added tag.js model

---
+++ src/Models/tag.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define the tag schema
+const tagSchema = new Schema({
+  type: {
+    type: String,
+    enum: ['Monuments', 'Museums', 'Religious Sites', 'Palaces/Castles'], // Restrict to predefined values
+    required: true,
+  },
+  historicPeriod: {
+    type: String,
+    enum: [
+      'Prehistoric', 'Ancient Egyptian', 'Roman Empire', 'Byzantine Empire', 
+      'Islamic Golden Age', 'Renaissance', 'Victorian Era', 'World War II', 
+      'Cold War', 'Digital Revolution', 'Space Age'
+    ], // Restrict to predefined historical periods
+    required: true,
+  },
+});
+
+const tag = mongoose.model('tag', tagSchema);
+module.exports = tag;
