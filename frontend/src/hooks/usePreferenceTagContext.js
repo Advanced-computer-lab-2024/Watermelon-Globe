3893a94a636d397d9eec2ea44f198f1314214cde
farah-khaled <farahkhaled03@gmail.com>
2024-10-07 00:49:30 +0300

    removed unnecessary frontend

--- frontend/src/hooks/usePreferenceTagContext.js
+++
@@ -1,9 +0,0 @@
-import { PreferenceTagContext } from "../context/PreferenceTagContext"
-import { useContext } from "react"
-export const usePreferenceTagContext = () => {
-  const context = useContext(PreferenceTagContext)
-  if(!context) {
-    throw Error('usePrefenceTagContext must be used inside an PrefenceTagContextContextProvider')
-  }
-  return context
-}
