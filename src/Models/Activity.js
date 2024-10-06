23c0018e8ecbcfa3773ef94cfad96569427a73e3
farah-khaled <farahkhaled03@gmail.com>
2024-10-06 15:02:10 +0300

    frontend dev-advertiser

---
+++ src/Models/Activity.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ActivitySchema = new Schema({
+  Date: {
+    type: Date,
+    required: true
+  },
+  Time: {
+    type: String,
+    required: true
+  },
+  Location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  },
+  Price: {
+    type: Number,
+    required: true
+  },
+  priceRange: {
+    type: [Number],
+    required: false
+  },
+  Category: {
+    type: String,
+    required: false
+  },
+  tags: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'tag'
+  }],
+  Discount: {
+    type: Number,
+    required: true
+  },
+  bookingOpen: {
+    type: Boolean,
+    default: true
+  },
+  Rating: {
+    type: Number,
+    required: false
+  },
+  Advertiser: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'CompanyProfile',
+    required: true
+  }
+}, { timestamps: true });
+
+ActivitySchema.index({ Location: '2dsphere' });
+
+const Activity = mongoose.model('Activity', ActivitySchema);
+module.exports = Activity;
---
+++ src/Models/tag.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tagSchema = new Schema({
+  type: {
+    type: String,
+    enum: ['Monuments', 'Museums', 'Religious Sites', 'Palaces/Castles'], 
+    required: true,
+  },
+  historicPeriod: {
+    type: String,
+    enum: [
+      'Prehistoric', 'Ancient Egyptian', 'Roman Empire', 'Byzantine Empire', 
+      'Islamic Golden Age', 'Renaissance', 'Victorian Era', 'World War II', 
+      'Cold War', 'Digital Revolution', 'Space Age'
+    ],
+    required: true,
+  },
+});
+
+const tag = mongoose.model('tag', tagSchema);
+module.exports = tag;
