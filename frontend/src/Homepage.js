54d0510255bf98bfb28b33cd8f305d73183fb0ef
Abuel3la <167132038+Abuel3la@users.noreply.github.com>
2024-10-07 16:32:18 +0300

    mved app to backend req and new homepage

---
+++ frontend/src/Homepage.js
@@ -0,0 +1,56 @@
+// src/HomePage.js
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Welcome to the Home Page</h1>
+      <button onClick={() => navigate('/BackendReq')}>
+        Go to App Page
+      </button>
+
+      <button onClick={() => navigate('/BackendReq')}>
+        Go to Guest Page
+      </button>
+
+      <button onClick={() => navigate('/BackendReq')}>
+        Go to Seller Page
+      </button>
+
+      <button onClick={() => navigate('/BackendReq')}>
+        Go to Advertiser Page
+      </button>
+
+      <button onClick={() => navigate('/BackendReq')}>
+        Go to Tourist Page
+      </button>
+
+      <button onClick={() => navigate('/BackendReq')}>
+        Go to Tour Guide Page
+      </button>
+
+      <button onClick={() => navigate('/BackendReq')}>
+        Go to Tourism Governor Page
+      </button>
+
+      <button onClick={() => navigate('/BackendReq')}>
+        Go to admin Page
+      </button>
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+};
+
+export default HomePage;
