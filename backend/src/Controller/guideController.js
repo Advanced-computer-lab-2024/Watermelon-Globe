1748938a1a1583e3d000968b59063dc0b87e1559
Hegawy <yusuf.hegawy@gmail.com>
2024-10-07 22:47:56 +0300

    Update guideController.js

--- backend/src/Controller/guideController.js
+++ backend/src/Controller/guideController.js
@@ -103,32 +103,6 @@ const deleteGuide = async (req, res) => {
     }
   };
 
-const deleteGuide = async (req, res) => {
-    const { id } = req.params; // Extract the site ID from the request parameters
-  
-    // Validate the site ID
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(400).json({ error: 'Invalid guide ID' });
-    }
-  
-    try {
-      // Attempt to find and delete the site by its ID
-      const deletedGuide = await TourGuide.findByIdAndDelete(id);
-  
-      // If no site was found with the provided ID, return a 404 error
-      if (!deletedGuide) {
-        return res.status(404).json({ message: 'Tourism site not found' });
-      }
-  
-      // Send a success message if the site was deleted
-      res.status(200).json({ message: 'Tourism site deleted successfully' });
-    } catch (error) {
-      // Handle any errors that occur
-      res.status(500).json({ error: error.message });
-    }
-  };
-
-
 const createItinerary = async (req, res) => {
     const { name, activities, tag, locations, timeline, languageOfTour, priceOfTour, availableDates, availableTimes,
         accessibility, pickupDropoffLocations, bookings, guide: guideId} = req.body;
