ad5282637ae9f98711366fa1bcda8f942e10f67c
Seif-waell <seifwael20122@outlook.com>
2024-11-11 02:11:33 +0200

    tazbet alwan shahd

--- frontend/src/Tourist/pages/TouristDetails.jsx
+++ frontend/src/Tourist/pages/TouristDetails.jsx
@@ -356,7 +356,7 @@ export default function TouristDetails() {
                 </div>
             )}
             <button onClick={() => setShowPasswordModal(true)} 
-          className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-200">
+          className="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-blue-700 transition duration-200">
           Change Password
           </button>
           {/* Password Change Modal */}
