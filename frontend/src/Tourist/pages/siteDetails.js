1d2a2ae769423b0382eff29f200a46d683329a23
Ahmed Hamdy <162977062+Medo4255@users.noreply.github.com>
2024-12-05 21:09:05 +0200

    Update siteDetails.js

--- frontend/src/Tourist/pages/siteDetails.js
+++ frontend/src/Tourist/pages/siteDetails.js
@@ -46,27 +46,12 @@ const SiteDetails = () => {
     <div className="min-h-screen bg-background py-8">
 
       <div className="max-w-4xl mx-auto bg-cardBackground shadow-md rounded-lg p-6">
-      <button
-  onClick={() => navigate(-1)}
-  className="focus:outline-none focus:ring-0 active:outline-none text-primary hover:text-secondaryHover transition duration-200"
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    className="lucide lucide-arrow-left w-6 h-6"
-  >
-    <path d="m12 19-7-7 7-7"></path>
-    <path d="M19 12H5"></path>
-  </svg>
-</button>
-
+        <button
+          onClick={() => navigate(-1)}
+          className="text-primary hover:text-secondaryHover transition duration-200 focus:outline-none focus-visible:outline-none focus:ring-0 active:outline-none"
+          >
+          <ArrowLeft className="w-6 h-6" />
+        </button>
         <h1 className="text-secondary text-4xl font-bold mb-4">{site.name}</h1>
         <p className="text-grayText text-lg mb-6">{site.description}</p>
         <img
