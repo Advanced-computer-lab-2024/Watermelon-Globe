0a9f33ff84ee6519ad981e2e82a5fc2b6d533df8
Omar-Seif <162040202+Omar-Seif@users.noreply.github.com>
2024-12-07 01:45:46 +0200

    commit

--- frontend/src/Admin++/pages/home/Home.jsx
+++ frontend/src/Admin++/pages/home/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
       <div className="homeContainerAdmin">
         <Navbar />
         <div className="widgetsAdminHome">
-          {/* <Widget type="user" /> */}
+          <Widget type="user" />
           <Widget type="product" />
           <Widget type="itinerary" />
           <Widget type="activity" />
