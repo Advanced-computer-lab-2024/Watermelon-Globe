a6292c16de4affe832c89b2903cda1e971eaabb7
Omar-Seif <162040202+Omar-Seif@users.noreply.github.com>
2024-12-07 20:40:42 +0200

    sidebar

--- frontend/src/TourGuide/Components/sidebar/Sidebar.jsx
+++ frontend/src/TourGuide/Components/sidebar/Sidebar.jsx
@@ -1,61 +1,3 @@
-
-
-// import "./sidebar.scss";
-// import { useParams, useLocation } from "react-router-dom";
-// import StoreIcon from "@mui/icons-material/Store";
-// import { Link } from "react-router-dom";
-// import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
-// import AddIcon from '@mui/icons-material/Add';
-// import CelebrationIcon from '@mui/icons-material/Celebration';
-
-// const Sidebar = () => {
-//   const { id } = useParams();
-//   const location = useLocation();
-
-//   // Function to determine if the menu item is active
-//   const isActive = (path) => location.pathname === path;
-
-//   return (
-//     <div className="sidebar">
-//       <div className="top">
-//         <Link to="/" style={{ textDecoration: "none" }}>
-//           <span className="logo">Tour Guide</span>
-//         </Link>
-//       </div>
-//       <hr />
-//       <div className="center">
-//         <ul>
-//           {/* My Products */}
-//           <Link to={`/ItineraryComponent2/${id}`} style={{ textDecoration: "none" }}>
-//             <li className={isActive(`/ItineraryComponent2/${id}`) ? "active" : ""}>
-//               <CelebrationIcon className="icon" />
-//               <span>My Itineraries</span>
-//             </li>
-//           </Link>
-
-//           {/* Create New Product */}
-//           <Link to={`/AllItineraries/${id}`} style={{ textDecoration: "none" }}>
-//             <li className={isActive(`/AllItineraries/${id}`) ? "active" : ""}>
-//               <CelebrationIcon className="icon" />
-//               <span>All Itineraries</span>
-//             </li>
-//           </Link>
-
-//           {/* Sales and Available Quantities */}
-//           <Link to={`/CreateItinerary/${id}`} style={{ textDecoration: "none" }}>
-//             <li className={isActive(`/CreateItinerary/${id}`) ? "active" : ""}>
-//               <AddIcon className="icon" />
-//               <span>Create an Itinerary</span>
-//             </li>
-//           </Link>
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Sidebar;
-
 import "./sidebar.scss";
 import { useParams, useLocation } from "react-router-dom";
 import StoreIcon from "@mui/icons-material/Store";
@@ -63,6 +5,7 @@ import { Link } from "react-router-dom";
 import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
 import AddIcon from '@mui/icons-material/Add';
 import CelebrationIcon from '@mui/icons-material/Celebration';
+import { TbLayoutDashboardFilled } from "react-icons/tb";
 
 const Sidebar = () => {
   const { id } = useParams();
@@ -72,19 +15,31 @@ const Sidebar = () => {
   const isActive = (path) => location.pathname === path;
 
   return (
-    <div className="sidebar-guide">
-      <div className="top-guide">
+    <div className="sidebarAdmin">
+      <div className="topAdmin">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo-guide">Tour Guide</span>
+          <span className="logoAdmin">Tour Guide</span>
         </Link>
       </div>
       <hr />
-      <div className="center-guide">
+      <div className="centerAdmin">
         <ul>
-          {/* My Products */}
+        <p className="titleAdmin">MAIN</p>
+
+        <Link
+            to={`/TourGuideHome/${id}`}
+            style={{ textDecoration: "none" }}
+          >
+            <li>
+              <TbLayoutDashboardFilled className="iconAdmin" />
+              <span>Dashboard</span>
+            </li>
+          </Link>
+
+          <p className="titleAdmin">MANAGE</p>
           <Link to={`/ItineraryComponent2/${id}`} style={{ textDecoration: "none" }}>
             <li className={isActive(`/ItineraryComponent2/${id}`) ? "active-guide" : ""}>
-              <CelebrationIcon className="icon-guide" />
+              <CelebrationIcon className="iconAdmin" />
               <span>My Itineraries</span>
             </li>
           </Link>
@@ -92,7 +47,7 @@ const Sidebar = () => {
           {/* All Itineraries */}
           <Link to={`/AllItineraries/${id}`} style={{ textDecoration: "none" }}>
             <li className={isActive(`/AllItineraries/${id}`) ? "active-guide" : ""}>
-              <CelebrationIcon className="icon-guide" />
+              <CelebrationIcon className="iconAdmin" />
               <span>All Itineraries</span>
             </li>
           </Link>
@@ -100,7 +55,7 @@ const Sidebar = () => {
           {/* Create New Itinerary */}
           <Link to={`/CreateItinerary/${id}`} style={{ textDecoration: "none" }}>
             <li className={isActive(`/CreateItinerary/${id}`) ? "active-guide" : ""}>
-              <AddIcon className="icon-guide" />
+              <AddIcon className="iconAdmin" />
               <span>Create an Itinerary</span>
             </li>
           </Link>
