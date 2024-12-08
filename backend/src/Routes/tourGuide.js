5995820525520d6e59689e7ce30a07b9fe6f1d4c
Ahmed Hamdy <162977062+Medo4255@users.noreply.github.com>
2024-12-08 16:00:43 +0200

    Revert "Merge branch 'malak-trial' of https://github.com/Advanced-computer-lab-2024/Watermelon-Globe into malak-trial"
    This reverts commit 4e6924a2d80b9c49f1e7ebcab301e61abb49018f, reversing
    changes made to 2cb4a3074f4160de4be4c9a1270403df41a58e1e.

--- backend/src/Routes/tourGuide.js
+++ backend/src/Routes/tourGuide.js
@@ -30,16 +30,4 @@ router.put(
   guideController.acceptTermsAndConditions
 );
 
-router.get("/getAllItineraries", guideController.getAllItineraries);
-
-router.get("/getAllItinerariesByGuide/:guideId", guideController.getAllItinerariesByGuide);
-
-router.get("/ItineraryRevenue/:guideId", guideController.ItineraryRevenue);
-
-router.get("/guideMonthlyRevenue/:guideId", guideController.guideMonthlyRevenue);
-
-router.get("/filterRevenueByDateGuide/:guideId", guideController.filterRevenueByDateGuide);
-
-
-
 module.exports = router;
