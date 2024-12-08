9b6af19d33b17d9ee3c841e92b031b7547eef675
malak-2003 <140625526+malak-2003@users.noreply.github.com>
2024-12-09 00:28:17 +0200

    12:28

--- frontend/src/Tourist/pages/SellerSignUp.js
+++ frontend/src/Tourist/pages/SellerSignUp.js
@@ -319,7 +319,7 @@ const SignUpSellerNew = () => {
             >
               Already have an account?{" "}
               <a
-                href="/tourist-signup"
+                href="/AllLogin"
                 style={{
                   color: "#d32e65",
                   fontWeight: 500,
